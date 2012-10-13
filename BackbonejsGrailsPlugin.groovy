2c940c43dabf9d42a36ee4e73ee1068ccd7e323d
Karol Balejko <kb@groovydev.com>
2012-10-13 21:08:04 +0200

    version bump up

--- BackbonejsGrailsPlugin.groovy
+++ BackbonejsGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class BackbonejsGrailsPlugin {
-    def version = "0.9.2"
+    def version = "0.9.2.1"
     def grailsVersion = "2.0 > *"
     def title = "Grails Backbone.js Resources Plugin"
     def author = "Karol Balejko"
